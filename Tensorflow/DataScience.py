c9e083da9247fb114a431c4f30da0179f705d552
aelgebaly-tgm <56672405+aelgebaly-tgm@users.noreply.github.com>
2022-01-27 11:33:49 +0100

    shuffle now available

--- Tensorflow/DataScience.py
+++ Tensorflow/DataScience.py
@@ -11,26 +11,43 @@ from sklearn.utils import shuffle
 import numpy as np
 import glob
 
+# https://stackoverflow.com/questions/4601373/better-way-to-shuffle-two-numpy-arrays-in-unison
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 
 
 def imageToArray(path):
-    img = load_img(path, color_mode = "grayscale")
+    img = load_img(path, color_mode="grayscale")
     print(img)
     img_array = img_to_array(img)
     img_np = np.array([img_array])
-    img_np = img_np.reshape(45,45)
+    img_np = img_np.reshape(45, 45)
     img_np /= 255.0
     return img_np
+
 
 ffilelist = glob.glob('Letters/f30/*.jpg')
 lfilelist = glob.glob('Letters/l30/*.jpg')
 
-fArray= np.array([imageToArray(fname) for fname in ffilelist])
-lArray= np.array([imageToArray(fname) for fname in lfilelist])
-letterArray = np.concatenate((fArray,lArray))
+fArray = np.array([imageToArray(fname) for fname in ffilelist])
+lArray = np.array([imageToArray(fname) for fname in lfilelist])
+letterArray = np.concatenate((fArray, lArray))
 print(letterArray.shape)
-labelArray = np.append(np.zeros((1, 30))[0] , np.ones((1,30))[0])
+labelArray = np.append(np.zeros((1, 30))[0], np.ones((1, 30))[0])
 
-array1_shuffled, array2_shuffled = sklearn.utils.shuffle(array1, array2)
+letterArray_shuffled, labelArray_shuffled = unison_shuffled_copies(letterArray, labelArray)
 
 
+print(labelArray_shuffled[0])
+plt.imshow(letterArray_shuffled[0], interpolation='nearest')
+plt.show()
+
+print(labelArray_shuffled[3])
+plt.imshow(letterArray_shuffled[3], interpolation='nearest')
+plt.show()
+
+print(labelArray_shuffled[20])
+plt.imshow(letterArray_shuffled[20], interpolation='nearest')
+plt.show()
